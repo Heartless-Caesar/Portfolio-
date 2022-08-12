8df08557dda6f5053640124452a094f2605c828e
caesar <joaovictorcsatu@gmail.com>
2022-08-12 11:01:48 -0300

    update

--- portfolio/assets/data/IDES.js
+++ portfolio/assets/data/IDES.js
@@ -3,13 +3,13 @@ const IDES = [
     img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vscode/vscode-original-wordmark.svg",
     docs: "https://code.visualstudio.com/docs",
     description:
-      "Visual Studio Code is a lightweight but powerful source code editor which runs on your desktop and is available for Windows, macOS and Linux. It comes with built-in support for JavaScript, TypeScript and Node.js and has a rich ecosystem of extensions for other languages and runtimes (such as C++, C#, Java, Python, PHP, Go, .NET).",
+      "Visual Studio Code is a lightweight but powerful source code editor which runs on your desktop and is available for Windows, macOS and Linux. ",
   },
   {
     img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/intellij/intellij-original.svg",
     docs: "https://www.jetbrains.com/idea/resources/",
     description:
-      "IntelliJ IDEA is an intelligent, context-aware IDE for working with Java and other JVM languages like Kotlin, Scala, and Groovy on all sorts of applications. Additionally, IntelliJ IDEA Ultimate can help you develop full-stack web applications, thanks to its powerful integrated tools, support for JavaScript and related technologies, and advanced support for popular frameworks like Spring, Spring Boot, Jakarta EE, Micronaut, Quarkus, Helidon. Moreover, you can extend IntelliJ IDEA with free plugins developed by JetBrains, allowing you to work with other programming languages, including Go, Python, SQL, Ruby, and PHP. ",
+      "IntelliJ IDEA is an intelligent, context-aware IDE for working with Java and other JVM languages like Kotlin, Scala, and Groovy on all sorts of applications. ",
   },
   {
     img: "https://resources.jetbrains.com/storage/products/company/brand/logos/Rider_icon.svg",
--- portfolio/pages/tecnologias.js
+++ portfolio/pages/tecnologias.js
@@ -1,308 +1,36 @@
 import React from "react";
 import Navbar from "../components/navbar";
-
+import IDES from "../assets/data/IDES";
 //TODO: MAKE ALL BUTTONS SAME WIDTH
 
 const Tecnologias = () => {
   return (
     <>
       <Navbar />
-      <div className="container text-light text-center display-5">
-        Essas são as tecnologias que tenho domínio suficiente para produzir
-        aplicações
+      <div className="container text-center display-3 text-light mb-3">
+        IDE 's
       </div>
-      <div className="grid-container text-light text-center mx-5 my-5 tech-div px-4 py-4">
-        <div className="row">
-          <div className="col">
-            <h3 className="display-3 my-5">IDE 's</h3>
-            <div className="items-container row">
-              <li className="list-item h4 col-6">
-                <a
-                  href="https://docs.microsoft.com/en-us/dotnet/csharp/"
-                  target="_blank"
-                >
-                  <button
-                    type="button"
-                    className="btn btn-dark btn-lg btn-block list-btn"
-                  >
-                    <img
-                      className="tech-img"
-                      src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vscode/vscode-original-wordmark.svg"
-                    />
-                  </button>
-                </a>
-              </li>
-              <li className="list-item h4 col-6">
-                <a
-                  href="https://docs.microsoft.com/en-us/dotnet/csharp/"
-                  target="_blank"
-                >
-                  <button
-                    type="button"
-                    className="btn btn-dark btn-lg btn-block list-btn"
-                  >
-                    <img
-                      className="tech-img"
-                      src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/intellij/intellij-original.svg"
-                    />
-                  </button>
-                </a>
-              </li>
-            </div>
-            <div className="row ">
-              <li className="list-item h4">
-                <a
-                  href="https://docs.microsoft.com/en-us/dotnet/csharp/"
-                  target="_blank"
-                >
-                  <button
-                    type="button"
-                    className="btn btn-dark btn-lg btn-block list-btn"
-                  >
-                    <img src="https://resources.jetbrains.com/storage/products/company/brand/logos/Rider_icon.svg" />
-                  </button>
-                </a>
-              </li>
-            </div>
-          </div>
-          <div className="col">
-            <h3 className="display-3 my-5">Databases</h3>
-            <div className="items-container">
-              <div className="row">
-                <li className="list-item h4 col-6">
-                  <a
-                    href="https://docs.microsoft.com/en-us/dotnet/csharp/"
-                    target="_blank"
-                  >
-                    <button
-                      type="button"
-                      className="btn btn-dark btn-lg btn-block list-btn"
-                    >
-                      <img
-                        className="tech-img"
-                        src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original-wordmark.svg"
-                      />
-                    </button>
-                  </a>
-                </li>
-                <li className="list-item h4 col-6">
-                  <a
-                    href="https://docs.microsoft.com/en-us/dotnet/csharp/"
-                    target="_blank"
-                  >
-                    <button
-                      type="button"
-                      className="btn btn-light btn-lg btn-block list-btn"
-                    >
-                      <img
-                        className="tech-img"
-                        src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original-wordmark.svg"
-                      />
-                    </button>
-                  </a>
-                </li>
+      <div className="card-container d-flex justify-content-center">
+        {IDES.map((i) => {
+          const { img, docs, description } = i;
+
+          return (
+            <div className="card project-div mx-5 p-4 mb-3">
+              <div className="card-img-top">
+                <img src={img} alt="IDE image" className="project-image pl-4" />
               </div>
-              <div className="row">
-                <li className="list-item h4 col-6">
-                  <a
-                    href="https://docs.microsoft.com/en-us/dotnet/csharp/"
-                    target="_blank"
-                  >
-                    <button
-                      type="button"
-                      className="btn btn-light btn-lg btn-block list-btn"
-                    >
-                      <img
-                        className="tech-img"
-                        src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/microsoftsqlserver/microsoftsqlserver-plain-wordmark.svg"
-                      />
-                    </button>
+              <div className="card-text">
+                <p className="mt-3 lead">
+                  You can view the documentation{" "}
+                  <a href={docs} target="_blank">
+                    here
                   </a>
-                </li>
-                <li className="list-item h4 col-6">
-                  <a
-                    href="https://docs.microsoft.com/en-us/dotnet/csharp/"
-                    target="_blank"
-                  >
-                    <button
-                      type="button"
-                      className="btn btn-dark btn-lg btn-block list-btn"
-                    >
-                      <img
-                        className="tech-img"
-                        src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original-wordmark.svg"
-                      />
-                    </button>
-                  </a>
-                </li>
+                </p>
+                <p className="lead">{description}</p>
               </div>
             </div>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col">
-            <h3 className="display-3 my-5">Programming Languages</h3>
-            <div className="items-container span2">
-              <div className="row">
-                <li className="list-item h4 col-6">
-                  <a
-                    href="https://docs.microsoft.com/en-us/dotnet/csharp/"
-                    target="_blank"
-                  >
-                    <button
-                      type="button"
-                      className="btn btn-light btn-lg btn-block list-btn"
-                    >
-                      <img
-                        className="tech-img"
-                        src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/csharp/csharp-original.svg"
-                      />
-                    </button>
-                  </a>
-                </li>
-                <li className="list-item h4 col-6">
-                  <a
-                    href="https://docs.microsoft.com/en-us/dotnet/csharp/"
-                    target="_blank"
-                  >
-                    <button
-                      type="button"
-                      className="btn btn-dark btn-lg list-btn"
-                    >
-                      <img
-                        className="tech-img"
-                        src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg"
-                      />
-                    </button>
-                  </a>
-                </li>
-              </div>
-              <div className="row">
-                <li className="list-item h4 col-6">
-                  <a
-                    href="https://docs.microsoft.com/en-us/dotnet/csharp/"
-                    target="_blank"
-                  >
-                    <button
-                      type="button"
-                      className="btn btn-dark btn-lg list-btn"
-                    >
-                      <img
-                        className="tech-img"
-                        src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg"
-                      />
-                    </button>
-                  </a>
-                </li>
-                <li className="list-item h4 col-6">
-                  <a
-                    href="https://docs.microsoft.com/en-us/dotnet/csharp/"
-                    target="_blank"
-                  >
-                    <button
-                      type="button"
-                      className="btn btn-light btn-lg btn-block list-btn"
-                    >
-                      <img
-                        className="tech-img"
-                        src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original-wordmark.svg"
-                      />
-                    </button>
-                  </a>
-                </li>
-              </div>
-            </div>
-          </div>
-          <div className="col">
-            <h3 className="display-3 my-5">Frameworks</h3>
-            <div className="items-container">
-              <div className="row" style={{ marginTop: "122.5px" }}>
-                <li className="list-item h4 col-6">
-                  <a
-                    href="https://docs.microsoft.com/en-us/dotnet/csharp/"
-                    target="_blank"
-                  >
-                    <button
-                      type="button"
-                      className="btn btn-light btn-lg btn-block list-btn"
-                    >
-                      <img
-                        className="tech-img"
-                        src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/dot-net/dot-net-plain-wordmark.svg"
-                      />
-                    </button>
-                  </a>
-                </li>
-                <li className="list-item h4 col-6">
-                  <a
-                    href="https://docs.microsoft.com/en-us/dotnet/csharp/"
-                    target="_blank"
-                  >
-                    <button
-                      type="button"
-                      className="btn btn-dark btn-lg btn-block list-btn"
-                    >
-                      <img
-                        className="tech-img"
-                        src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg"
-                      />
-                    </button>
-                  </a>
-                </li>
-              </div>
-              <div className="row">
-                <li className="list-item h4 col-6">
-                  <a
-                    href="https://docs.microsoft.com/en-us/dotnet/csharp/"
-                    target="_blank"
-                  >
-                    <button
-                      type="button"
-                      className="btn btn-light btn-lg btn-block list-btn"
-                    >
-                      <img
-                        className="tech-img"
-                        src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original-wordmark.svg"
-                      />
-                    </button>
-                  </a>
-                </li>
-                <li className="list-item h4 col-6">
-                  <a
-                    href="https://docs.microsoft.com/en-us/dotnet/csharp/"
-                    target="_blank"
-                  >
-                    <button
-                      type="button"
-                      className="btn btn-dark btn-lg btn-block list-btn"
-                    >
-                      <img
-                        className="tech-img"
-                        src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original-wordmark.svg"
-                      />
-                    </button>
-                  </a>
-                </li>
-              </div>
-              <li className="list-item h4 mt-2">
-                <a
-                  href="https://docs.microsoft.com/en-us/dotnet/csharp/"
-                  target="_blank"
-                >
-                  <button
-                    type="button"
-                    className="btn btn-light btn-lg btn-block list-btn"
-                  >
-                    <img
-                      className="tech-img"
-                      src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original-wordmark.svg"
-                    />
-                  </button>
-                </a>
-              </li>
-            </div>
-          </div>
-        </div>
+          );
+        })}
       </div>
     </>
   );

8228ce040345bca84b3b709e93bce0d9d6ad2d9f
caesar <joaovictorcsatu@gmail.com>
2022-08-13 00:53:28 -0300

    update

---
+++ portfolio/components/databases.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import databases from "../assets/data/databases";
+const Databases = () => {
+  return (
+    <>
+      <div
+        className="text-center display-3 text-light mx-5"
+        style={{ marginTop: "10%" }}
+      >
+        Bancos de dados
+      </div>
+      <div className="d-flex justify-content-center flex-wrap">
+        {databases.map((i) => {
+          const { img, docs, description } = i;
+
+          return (
+            <div className="card project-div p-3 m-5">
+              <div className="card-img-top text-center">
+                <img src={img} alt="IDE image" className="tech-img pl-4" />
+              </div>
+              <div className="card-text">
+                <p className="mt-3 lead">
+                  You can view the documentation{" "}
+                  <a href={docs} target="_blank">
+                    here
+                  </a>
+                </p>
+                <p className="lead">{description}</p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+};
+
+export default Databases;
--- portfolio/pages/tecnologias.js
+++ portfolio/pages/tecnologias.js
@@ -1,37 +1,15 @@
 import React from "react";
 import Navbar from "../components/navbar";
-import IDES from "../assets/data/IDES";
+import IDE from "../components/ides";
+import Databases from "../components/databases";
 //TODO: MAKE ALL BUTTONS SAME WIDTH
 
 const Tecnologias = () => {
   return (
     <>
       <Navbar />
-      <div className="container text-center display-3 text-light mb-3">
-        IDE 's
-      </div>
-      <div className="card-container d-flex justify-content-center">
-        {IDES.map((i) => {
-          const { img, docs, description } = i;
-
-          return (
-            <div className="card project-div mx-5 p-4 mb-3">
-              <div className="card-img-top">
-                <img src={img} alt="IDE image" className="project-image pl-4" />
-              </div>
-              <div className="card-text">
-                <p className="mt-3 lead">
-                  You can view the documentation{" "}
-                  <a href={docs} target="_blank">
-                    here
-                  </a>
-                </p>
-                <p className="lead">{description}</p>
-              </div>
-            </div>
-          );
-        })}
-      </div>
+      <IDE />
+      <Databases />
     </>
   );
 };

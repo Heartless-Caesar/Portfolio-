810c6edf0ef506b8807a36696a1786c065000c78
caesar <joaovictorcsatu@gmail.com>
2022-08-17 11:06:54 -0300

    update

--- portfolio/components/body.jsx
+++ portfolio/components/body.jsx
@@ -1,6 +1,7 @@
 import React from "react";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
 import it from "../assets/images/IT_clip_art.png";
-
 const Body = () => {
   return (
     <div className="text-dark bg-light body-container">
@@ -9,23 +10,21 @@ const Body = () => {
           <h3 className="display-4 mt-3">John Victor Cesar</h3>
           <div className="content-body col-8">
             <p className="lead about-me">
-              I'm an aspiring software developer who's got experience in
+              I'm an aspiring software developer who&aposs got experience in
               projects, of all scopes, as a freelancer.
               <br />
-              The stacks I'm more comfortable with are Next JS & Node JS but
-              there are more frameworks I have practice with.{" "}
+              The stacks I &aposm more comfortable with are Next JS & Node JS
+              but there are more frameworks I have practice with.{" "}
             </p>
           </div>
-          <div className="col-4">
-            <img
-              src="https://hdclipartall.com/images/technology-clipart-technology-clipart-infomation-11-335x202.png"
+          {/* {<div className={styles.imageContainer}>
+            <Image
+              src={it}
               alt="image-here"
-              style={{
-                height: "200px",
-                marginLeft: "90px",
-              }}
+              layout="fill"
+              className={styles.image}
             />
-          </div>
+          </div>} */}
         </div>
       </main>
     </div>
